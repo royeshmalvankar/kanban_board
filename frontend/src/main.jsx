20a4e0854338bf6c21ce37e9f1b563c688431241
royeshmalvankar <royeshmalvankar@gmail.com>
2024-08-28 13:29:24 +0530

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AuthContextProvider } from './authcontext/AuthContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
     <AuthContextProvider>
     <BrowserRouter>
     <ChakraProvider>
@@ -15,5 +13,4 @@ createRoot(document.getElementById('root')).render(
     </ChakraProvider>
     </BrowserRouter>
     </AuthContextProvider>
-  </StrictMode>,
 )
