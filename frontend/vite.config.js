f7f50d8f2b7cd89e206b58d33bd3fcc96d02bba1
royeshmalvankar <royeshmalvankar@gmail.com>
2024-08-28 13:00:57 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/movie': {
+        target: 'http://localhost:3000', // The backend server URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/board/, ''),
+      },
+    },
+  },
 })
